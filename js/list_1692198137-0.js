3c61fe603d63d28b4b2cdda503912866cae9c70d
XiX_Mega_W <xixmegaw@gmail.com>
2023-12-02 19:19:55 -0400

    Add files via upload

---
+++ js/list_1692198137-0.js
@@ -0,0 +1,583 @@
+const d = {
+    "list": [
+      /*=================================================================================*/
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          },
+      ],
+        "name": "spaceUK never clear",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3065,
+        "dif" : "Extreme Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 41,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 0
+      },
+      {
+        "vids": [
+          {
+            "user": "na",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 0,
+            "hz": "nahz"
+          },
+      ],
+        "name": "Took Circles",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 56,
+        "dif" : "Insane Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 57,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 3
+      },
+      {
+        "vids": [
+          {
+            "user": "Pekar",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "165hz"
+          },
+      ],
+        "name": "WHO",
+        "author": "Menew9",
+        "author2": "by zZer0 and wrmn",
+        "more": "none",
+        "id": 3200,
+        "dif" : "Extreme Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 60,
+        "verificationVid": "https://www.youtube.com/watch?v=EF9Hiko-kqo",
+        "key": 1
+      },
+      {
+        "vids": [
+          {
+            "user": "na",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 0,
+            "hz": "60hz"
+          },
+      ],
+        "name": "Doomzone",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3253,
+        "dif" : "Insane Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 64,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 2
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          },
+      ],
+        "name": "WALKING IN LIMBO",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3019,
+        "dif" : "Insane Demon",
+        "pass": "123456",
+        "extend": "Main",
+        "percentToQualify": 68,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 4
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)",
+          },
+          {
+            "user": "Pekar",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "165hz"
+          }
+      ],
+        "name": "Freeze",
+        "author": "idk who it is lol",
+        "author2": "by zZer0",
+        "more": "none",
+        "id": 3130,
+        "dif" : "Insane Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 54,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 5
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          },
+          {
+            "user": "XiXMegaW",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 65,
+            "hz": "165hz"
+          }
+      ],
+        "name": "Tunnel of Heck",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3130,
+        "dif" : "Insane Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 63,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 6
+      },
+      {
+        "vids": [
+          {
+            "user": "na",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 0,
+            "hz": "nahz"
+          },
+      ],
+        "name": "Dont beat this level",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 53,
+        "dif" : "Insane Demon",
+        "pass": "Free Copy",
+        "extend": "Main",
+        "percentToQualify": 57,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 7
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Schizophrenia",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3043,
+        "dif" : "Hard Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 71,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 8
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Black Hole",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3058,
+        "dif" : "Hard Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 53,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 9
+      },
+      {
+        "vids": [
+          {
+            "user": "na",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 0,
+            "hz": "nahz"
+          }
+      ],
+        "name": "Dont pick this level",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 54,
+        "dif" : "Hard Demon",
+        "pass": "Free Copy",
+        "extend": "Main",
+        "percentToQualify": 70,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 10
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "TIME MACHINE",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 54,
+        "dif" : "Hard Demon",
+        "pass": "Free Copy",
+        "extend": "Main",
+        "percentToQualify": 67,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 11
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Cobalt Depths",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3022,
+        "dif" : "Hard Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 57,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 12
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "LINP v1",
+        "author": "Guernsey",
+        "author2": "",
+        "more": "none",
+        "id": 3137,
+        "dif" : "Hard Demon",
+        "pass": "No Copy",
+        "extend": "Main",
+        "percentToQualify": 62,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 13
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)",
+          }
+      ],
+        "name": "Make It Drop",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3026,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 14
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)",
+          },
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Epic Rated Level",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3010,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 15
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "THE RACE",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3218,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 16
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "MINDCONTROLDELETE",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3131,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 17
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)",
+          }
+      ],
+        "name": "Surface",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3198,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 18
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Light Night",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3051,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 19
+      },
+      {
+        "vids": [
+          {
+            "user": "Menew9",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          },
+          {
+
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Off The Meds",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 19,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 20
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Polterwave",
+        "author": "Menew9",
+        "author2": "",
+        "more": "none",
+        "id": 3136,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 21
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Gimmick Adventure",
+        "author": "BillyGd986",
+        "author2": "",
+        "more": "none",
+        "id": 3073,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 22
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Iron God",
+        "author": "TheRealAragorn",
+        "author2": "",
+        "more": "none",
+        "id": 3120,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 23
+      },
+      {
+        "vids": [
+          {
+            "user": "na",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 0,
+            "hz": "nahz"
+          }
+      ],
+        "name": "dont rate this level",
+        "author": "zZer0",
+        "author2": "",
+        "more": "none",
+        "id": 3049,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "key": 24
+      },
+      {
+        "vids": [
+          {
+            "user": "zZer0",
+            "link": "https://pastebin.com/dc2FhD4z",
+            "percent": 100,
+            "hz": "60hz (Mobile)"
+          }
+      ],
+        "name": "Beating Heart",
+        "author": "FZXXR",
+        "author2": "",
+        "more": "none",
+        "id": 3021,
+        "dif" : "Medium Demon",
+        "pass": "No Copy",
+        "extend": "Extended",
+        "percentToQualify": 100,
+        "verificationVid": "https://pastebin.com/dc2FhD4z",
+        "legacykey": 0,
+      }
+ ],
+ "version": [
+    2,
+    0,
+    0
+  ]
+};
+
+const list = d.list; const version = d.version;
---
+++ player.js
@@ -0,0 +1,6 @@
+const d = {
+    "user": "c1xa",
+    "link": "https://youtu.be/k1qneN1WhTk"
+    "percent": 100
+    "hz": "360"
+},
